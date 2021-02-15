86d5a1495bcae16fa5542c248b96d156112dd374
alansoldar <alan.soledar@hotmail.com>
2021-02-15 17:31:10 -0300

    fixing method name

--- avalia_astar_h1.py
+++ avalia_astar_h1.py
@@ -1,5 +1,20 @@
 import sys
 
+DIREITA = 'direita'
+ESQUERDA = 'esquerda'
+ACIMA = 'acima'
+ABAIXO = 'abaixo'
+
+class Node:
+    def __init__(self, initialState, direction, cost, newState):
+        self.initialState = initialState
+        self.direction = direction
+        self.cost = cost
+        self.newState = newState
+
+    
+
+
 def expande():
     puzzle = str(sys.argv[1])
     cost = int(sys.argv[2])
@@ -11,28 +26,28 @@ def expande():
 def getRight(puzzle, emptyPos, cost):
     if(emptyPos!=2 and emptyPos!=5 and emptyPos!=8):
         newState = puzzle[:emptyPos] + puzzle[emptyPos+1] + '_' + puzzle[emptyPos+2:]
-        return '(direita,'+ newState+','+ str(cost+1) +',' + puzzle+ ')'
+        return '(' + DIREITA + ','+ newState+','+ str(cost+1) +',' + puzzle+ ')'
     else:
         return ''
         
 def getLeft(puzzle, emptyPos, cost):
     if(emptyPos!=0 and emptyPos!=3 and emptyPos!=6):
         newState = puzzle[:emptyPos-1] + '_' + puzzle[emptyPos-1] + puzzle[emptyPos+1:]
-        return '(esquerda,'+ newState+','+ str(cost+1)+','+ puzzle+ ')'
+        return '(' + ESQUERDA + ',' + newState+','+ str(cost+1)+','+ puzzle+ ')'
     else:
         return ''
 
-def getUp(puzzle, emptyPos, cost):
+def geUp(puzzle, emptyPos, cost):
     if(emptyPos-3 >= 0):
         newState = puzzle[:emptyPos-3] + '_' + puzzle[emptyPos-2:emptyPos] + puzzle[emptyPos-3] + puzzle[emptyPos+1:]
-        return '(acima,'+ newState+','+ str(cost+1)+','+ puzzle + ')'
+        return '(' + ACIMA + ',' + newState+','+ str(cost+1)+','+ puzzle + ')'
     else:
         return ''
 
 def getDown(puzzle, emptyPos, cost):
     if(emptyPos+3 <= 8):
         newState = puzzle[:emptyPos] + puzzle[emptyPos+3] + puzzle[emptyPos+1:emptyPos+3] + '_' + puzzle[emptyPos+4:]
-        return '(abaixo,'+ newState+','+ str(cost+1)+','+ puzzle+ ')'
+        return '(' + ABAIXO + ',' + newState+','+ str(cost+1)+','+ puzzle+ ')'
     else:
         return ''
 
